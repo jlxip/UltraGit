4ef1d63c7850b41dcae9d17ba3c306354b8a5fcb
jlxip <jlxip.mc@gmail.com>
2017-12-02 19:11:27 +0100

    Seems like it's working :ghost:

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var dbconnection = require('./dbconnection.js')
 var getUsers = require('./getUsers.js')
-var getRepos = require('./getRepos.js')
+// FUTURE: var getRepoData = require('./getRepoData.js')
+var getPermissions = require('./getPermissions.js')
 var Server = require('node-git-server')
 var fs = require('fs')
 
@@ -36,51 +37,37 @@ exports.UltraGitServer = class {
 			const repos = new Server(reposPath, {
 				autoCreate: false,
 				authenticate: (type, repo, user, pwd, next) => {
-					console.log(type)
 					return new Promise((resolve, reject) => {
 						getUsers.getUsers(this.db, (users) => {
-							var gotit = false
 							for(var i=0;i<users.length;++i) {
 								if(users[i].user == user && users[i].pwd == pwd) {
-									gotit = true
+									return resolve()
 								}
 							}
-	
-							if(gotit) {
-								console.log(user+':'+pwd+' is doing '+type+' on '+repo) // DEBUG
-								return resolve()
-							}
+
 							return reject('Invalid username or password.')
 						})
 					})
 				}
 			})
 
-			repos.on('push', (push) => {
-				console.log('Doing push')
-				push.accept()
+			repos.on('fetch', (fetch) => {
+				getPermissions.isReadable(this.db, fetch.username, fetch.repo, (r) => {
+					if(r) fetch.accept()
+					else fetch.reject()	// This prints in the client-side an awful error.
+				})
 			})
 
-			repos.on('fetch', (fetch) => {
-				console.log('Doing fetch')
-				fetch.accept()
+			repos.on('push', (push) => {
+				getPermissions.isWritable(this.db, push.username, push.repo, (r) => {
+					if(r) push.accept()
+					else push.reject()
+				})
 			})
 
 			repos.listen(port, () => {
-				console.log(`Starting server at http://localhost:${port}`)
+				console.log(`Server running at http://localhost:${port}`)
 			})
-			/*getUsers.getUsers(this.db, (users) => {
-				getRepos.getRepos(this.db, users, (repos) => {
-					var opts = {
-						repos: repos,
-						port: port,
-						repoLocation: reposPath,
-						logging: true
-					}
-
-					new GitServer(opts)
-				})
-			})*/
 		})
 	}
 }
